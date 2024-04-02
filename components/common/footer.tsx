0ac69170be6a3690ea475b5c557d00eff1bc5882
Tim Brown <brown.tim@gmail.com>
2024-04-02 16:26:30 -0500

    adding footer info

--- components/common/footer.tsx
+++ components/common/footer.tsx
@@ -1,8 +1,19 @@
-import { FunctionComponent } from 'react';
-import styles from './footer.module.scss';
+import { FunctionComponent } from "react";
+import styles from "./footer.module.scss";
+import Link from "next/link";
 
 const Footer: FunctionComponent = () => {
-  return <footer className={styles.footer}></footer>;
+  return (
+    <footer className={styles.footer}>
+      <Link href={"https://photos.dinodev.hu"}>
+        application credit: photos.dinodev.hu
+      </Link>
+      {"  |  "}
+      <Link href={"/privacy"}>privacy information</Link>
+      {"  |  "}
+      <Link href={"/tos"}>terms of service</Link>
+    </footer>
+  );
 };
 
 export default Footer;
