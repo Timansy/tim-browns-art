ae8eb81cc98b448262f1ac95a918120f3f90b416
Tim Brown <brown.tim@gmail.com>
2025-09-18 13:50:36 -0400

    Removed footer

--- components/common/footer.tsx
+++ components/common/footer.tsx
@@ -5,13 +5,13 @@ import Link from "next/link";
 const Footer: FunctionComponent = () => {
   return (
     <footer className={styles.footer}>
-      <Link href={"https://photos.dinodev.hu"}>
+      {/* <Link href={"https://photos.dinodev.hu"}>
         application credit: photos.dinodev.hu
       </Link>
       {"  |  "}
       <Link href={"/privacy"}>privacy information</Link>
       {"  |  "}
-      <Link href={"/tos"}>terms of service</Link>
+      <Link href={"/tos"}>terms of service</Link> */}
     </footer>
   );
 };
